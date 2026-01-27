b92c72d6948d1e1e8342dad5514ed47cb68ebe4a
Dimple-Saini06 <dimple13619saini@gmail.com>
2026-01-27 21:56:07 +0530

    pattern

--- Pattern.java
+++ Pattern.java
@@ -1,15 +1,19 @@
 public class Pattern{
     public static void main(String args[]){
-       int rows = 10;
-       //outer loop
-       for(int i=1;i<=rows;i++){
-            //inner loop
+        int row = 3;
+        //outer loop
+        for(int i=1; i<=row; i++){
+            //inner loop (space)
+            for(int j=1; j<=row-i; j++){
+                System.out.print(" ");
+            }
+
+            //inner loop(star)
             for(int j=1; j<=i; j++){
-                System.out.print(j + " ");
+                System.out.print("*");
             }
 
             System.out.println();
-       }
+        }
     }
-
 }
