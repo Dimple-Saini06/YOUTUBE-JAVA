bc15fe634b1b9bf3f2d64b885414d13a281eb973
Dimple-Saini06 <dimple13619saini@gmail.com>
2026-01-29 20:01:50 +0530

    FUNCTION

---
+++ Function.java
@@ -0,0 +1,8 @@
+public class Function {
+    public static void doubleValue(int num){
+        return num *2;
+    }
+    public static void main(String args[]){
+        System.out.print(doubleValue(5)); //no print
+    }
+}
