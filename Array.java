3e025cf22c4ef6dc570a424db0ccef36241603d6
Dimple-Saini06 <dimple13619saini@gmail.com>
2026-04-26 21:08:56 +0530

    ..

---
+++ Array.java
@@ -0,0 +1,13 @@
+public class Array {
+    public static void main(String args[]){
+        int number[] = new int[5]; ///empty approach 
+
+        char val[] = {'a', 'b', 'c'};
+
+        // System.out.println(val[0]);  //a
+        // System.out.println(val[1]);
+        // System.out.println(val[3]);
+
+        System.out.println(val.length);
+    }
+}
